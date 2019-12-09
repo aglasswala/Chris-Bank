928da59dc740237f0a12e6019fdd529837f96fe1
Aadil Glasswala <aglasswala@gmail.com>
2019-12-08 19:40:54 -0500

    login updates'

--- client/src/components/sections/Hero.js
+++ client/src/components/sections/Hero.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Grid, Typography, withStyles, Button, Paper } from '@material-ui/core'
+import { Grid, Typography, withStyles, Button, Paper, FormControl, Input, InputLabel } from '@material-ui/core'
 import heroStyles from '../styles/heroStyles';
 import { ScrollDownIndicator } from 'react-landing-page'
 import BackgroundSlider from 'react-background-slider'
@@ -42,7 +42,7 @@ class Hero extends Component {
                                     </Typography>
                                 </Grid>
                                 <Grid item>
-                                    <div className={classes.wrapper}>
+                                    <div className={classes.wrapper1}>
                                         <Typography
                                             variant="h5"
                                             color="primary"
@@ -54,7 +54,7 @@ class Hero extends Component {
                                     </div>
                                 </Grid>
                                 <Grid item>
-                                    <div className={classes.wrapper}>
+                                    <div className={classes.wrapper1}>
                                         <NavLink
                                             to="register"
                                             style={{textDecoration: "none"}}
@@ -68,8 +68,56 @@ class Hero extends Component {
                             </Grid>
                         </Grid>
                         <Grid item>
-                            <Paper style={{width: "50vw", margin: 50}}>
-                                YES
+                            <Paper style={{width: "30vw", height: "100%", margin: 50, boxShadow: "0 8px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}}>
+                                <Typography
+                                    variant="h5"
+                                    color="primary"
+                                    align="center"
+                                    className={classes.heroText1}
+                                >
+                                    Sign in
+                                </Typography>
+                                <div className={classes.outer}>
+                                   <form
+                                       className={classes.form}
+                                   >
+                                       <span className={classes.wrapper}>
+                                           <FormControl margin="normal" fullWidth required>
+                                               <InputLabel> Email Address </InputLabel>
+                                               <Input 
+                                                   id="email" 
+                                                   name="email" 
+                                                   type="text"
+                                                   autoComplete="email" 
+                                                   autoFocus
+                                                   className={classes.textField}
+                                               />
+                                           </FormControl>
+                                       </span>
+                                       <span className={classes.wrapper}>
+                                           <FormControl margin="normal" fullWidth required>
+                                               <InputLabel htmlFor="password"> Password </InputLabel>
+                                               <Input 
+                                                   id="password" 
+                                                   name="password" 
+                                                   type="password"
+                                                   autoComplete="password"
+                                                   className={classes.textField}
+                                               />
+                                           </FormControl>
+                                       </span>
+                                       <span className={classes.wrapper}>
+                                           <Button
+                                               className={classes.button}
+                                               type="submit"
+                                               variant="contained"
+                                               color="primary"
+                                           >
+                                               Submit
+                                           </Button>
+                                       </span>
+                                   </form>
+                                </div>
                             </Paper>
                         </Grid>
                     </Grid>
